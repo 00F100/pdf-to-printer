6b87a638ef4c7a9ed043de315edaec3c89a35bc3
João Moraes <joaomoraesbr@gmail.com>
2023-04-19 11:28:56 -0300

    fix undefined for node <= 16.13

--- src/utils/windows-printer-valid.ts
+++ src/utils/windows-printer-valid.ts
@@ -20,6 +20,11 @@ export default function isValidPrinter(printer: string): {
   printer.split(/\r?\n/).forEach((line) => {
     let [label, value] = line.split(":").map((el) => el.trim());
 
+    // Fix value undefined on node =< 16.13
+    if (value == undefined) {
+      value = String();
+    }
+
     // handle array dots
     if (value.match(/^{(.*)(\.{3})}$/)) {
       value = value.replace("...}", "}");
