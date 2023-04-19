156d6eddeff671b0320dc41a503b993a1ba39253
João Moraes <joaomoraesbr@gmail.com>
2023-04-19 15:37:29 -0300

    fix code

--- src/utils/windows-printer-valid.ts
+++ src/utils/windows-printer-valid.ts
@@ -17,52 +17,38 @@ export default function isValidPrinter(printer: string): {
     paperSizes: [],
   };
 
-  let ignoreNext: boolean = false;
-
   const lines = printer.split(/\r?\n/);
 
   lines.forEach((line, index) => {
-    if (ignoreNext) {
-      ignoreNext = false;
-    } else {
-      let [label, value] = line.split(":").map((el) => el.trim());
+    let [label, value] = line.split(":").map((el) => el.trim());
 
-      if (value == undefined) {
-        value = "";
-      }
-
-      if (label == "PrinterPaperNames" && value.slice(-1) != "}") {
-        value = `${value}}`;
-      }
-
-      // handle array dots
-      if (value.match(/^{(.*)(\.{3})}$/)) {
-        value = value.replace("...}", "}");
-      }
-
-      // handle array returns
-      const matches = value.match(/^{(.*)}$/);
-
-      if (matches && matches[1]) {
-        // @ts-ignore
-        value = matches[1].split(", ");
-      }
-
-      if (label == "PrinterPaperNames") {
-        console.log("value", {
-          label,
-          value,
-          matches,
-        });
-      }
-
-      const key = properties[label];
-
-      if (key === undefined) return;
-
-      // @ts-ignore
-      printerData[key] = value;
+    if (value == undefined) {
+      value = "";
     }
+
+    if (label == "PrinterPaperNames" && value.slice(-1) != "}") {
+      value = `${value}}`;
+    }
+
+    // handle array dots
+    if (value.match(/^{(.*)(\.{3})}$/)) {
+      value = value.replace("...}", "}");
+    }
+
+    // handle array returns
+    const matches = value.match(/^{(.*)}$/);
+
+    if (matches && matches[1]) {
+      // @ts-ignore
+      value = matches[1].split(", ");
+    }
+
+    const key = properties[label];
+
+    if (key === undefined) return;
+
+    // @ts-ignore
+    printerData[key] = value;
   });
 
   const isValid = !!(printerData.deviceId && printerData.name);
